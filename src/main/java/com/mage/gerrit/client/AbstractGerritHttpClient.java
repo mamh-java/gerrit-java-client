5b2ec8d9cd7ec97d8d32fbf39469ac6b2e1ea513
mamh <bright.ma@blackshark.com>
2018-08-31 10:55:58 +0800

    java: add AbstractGerritHttpClient.java supper class
    Signed-off-by: mamh <bright.ma@blackshark.com>

---
+++ src/main/java/com/mage/gerrit/client/AbstractGerritHttpClient.java
@@ -0,0 +1,200 @@
+package com.mage.gerrit.client;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mage.gerrit.utils.UrlUtils;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScheme;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Arrays;
+
+public abstract class AbstractGerritHttpClient implements GerritHttpConnection {
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    protected static final String MAGIC_PREFIX = ")]}'\n";
+    protected static final String JSON_MIME_TYPE = ContentType.APPLICATION_JSON.getMimeType();
+
+    protected String context;
+    protected HttpContext localContext;
+
+
+    protected URI uri;
+    protected CloseableHttpClient client;
+    protected ObjectMapper mapper;
+
+
+    public CloseableHttpClient getClient() {
+        return client;
+    }
+
+    public void setClient(CloseableHttpClient client) {
+        this.client = client;
+    }
+
+    /**
+     * Get the local context.
+     *
+     * @return context
+     */
+    protected HttpContext getLocalContext() {
+        return localContext;
+    }
+
+
+    /**
+     * Set the local context.
+     *
+     * @param localContext the context
+     */
+    protected void setLocalContext(final HttpContext localContext) {
+        this.localContext = localContext;
+    }
+
+
+    protected void setRequestBody(String requestBody, HttpRequestBase method) {
+        if (requestBody != null) {
+            ((HttpEntityEnclosingRequestBase) method).setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+        }
+    }
+
+    protected HttpResponse request(String path, String verb, String requestBody, Header... headers) throws IOException {
+        HttpRequestBase method;
+        URI uri = UrlUtils.toJsonApiUri(this.uri, context, path);
+        switch (verb) {
+            case HttpPost.METHOD_NAME:
+                method = new HttpPost(uri);
+                setRequestBody(requestBody, method);
+                break;
+            case HttpGet.METHOD_NAME:
+                method = new HttpGet(uri);
+                break;
+            case HttpDelete.METHOD_NAME:
+                method = new HttpDelete(uri);
+                break;
+            case HttpPut.METHOD_NAME:
+                method = new HttpPut(uri);
+                setRequestBody(requestBody, method);
+                break;
+            default:
+                throw new IllegalStateException("Unknown or unsupported Http method: " + verb);
+        }
+
+        for (Header header : headers) {
+            method.addHeader(header);
+        }
+
+        HttpResponse response = client.execute(method, localContext);
+
+        checkResponse(response);
+
+
+        return response;
+    }
+
+    //从response 中获取 一个字节数组，去除掉json开头的魔术字符串
+    protected byte[] getResponseBytes(HttpResponse response) throws IOException {
+        InputStream content = response.getEntity().getContent();
+        byte[] bytes = IOUtils.toByteArray(content);
+        bytes = Arrays.copyOfRange(bytes, MAGIC_PREFIX.length(), bytes.length);
+        return bytes;
+    }
+
+    protected void checkResponse(HttpResponse response) throws HttpResponseException {
+        int status = response.getStatusLine().getStatusCode();
+        if (status < 200 || status >= 400) {
+            throw new HttpResponseException(status, response.getStatusLine().getReasonPhrase());
+        }
+        HttpEntity entity = response.getEntity();
+
+        Header contentType = entity.getContentType();
+        if (contentType != null && !contentType.getValue().contains(JSON_MIME_TYPE)) {
+            throw new HttpResponseException(status, String.format("Expected JSON but got '%s'.", contentType.getValue()));
+        }
+    }
+
+
+    protected ObjectMapper getDefaultMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        return mapper;
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        try {
+            client.close();
+        } catch (final IOException ex) {
+            LOGGER.debug("I/O exception closing client", ex);
+        }
+    }
+
+    protected static HttpClientBuilder addAuth(final HttpClientBuilder builder, final URI uri, final String username, String password) {
+        if (StringUtils.isNotBlank(username)) {
+            CredentialsProvider provider = new BasicCredentialsProvider();
+            AuthScope scope = new AuthScope(uri.getHost(), uri.getPort(), "realm");
+            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+            provider.setCredentials(scope, credentials);
+            builder.setDefaultCredentialsProvider(provider);
+
+            builder.addInterceptorFirst(new PreemptiveAuth());
+        }
+        return builder;
+    }
+
+    protected static class PreemptiveAuth implements HttpRequestInterceptor {
+
+        @Override
+        public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+            AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
+
+            if (authState.getAuthScheme() == null) {
+                AuthScheme authScheme = (AuthScheme) context.getAttribute("preemptive-auth");
+                CredentialsProvider credsProvider = (CredentialsProvider) context
+                        .getAttribute(ClientContext.CREDS_PROVIDER);
+                HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+                if (authScheme != null) {
+                    Credentials creds = credsProvider
+                            .getCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort()));
+                    if (creds == null) {
+                        throw new HttpException("No credentials for preemptive authentication");
+                    }
+                    authState.update(authScheme, creds);
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/mage/gerrit/client/GerritHttpClient.java
+++ src/main/java/com/mage/gerrit/client/GerritHttpClient.java
@@ -1,61 +1,23 @@
 package com.mage.gerrit.client;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mage.gerrit.model.BaseModel;
-import com.mage.gerrit.utils.UrlUtils;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScheme;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.AuthState;
-import org.apache.http.auth.Credentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.protocol.ClientContext;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.ExecutionContext;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class GerritHttpClient implements GerritHttpConnection {
-    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    private String context;
-    private static final String JSON_MIME_TYPE = ContentType.APPLICATION_JSON.getMimeType();
-
-    private URI uri;
-    private CloseableHttpClient client;
-    private HttpContext localContext;
-    private ObjectMapper mapper;
-
-    private static final String MAGIC_PREFIX = ")]}'\n";
+public class GerritHttpClient extends AbstractGerritHttpClient implements GerritHttpConnection {
 
     public GerritHttpClient(URI uri) {
         this(uri, HttpClientBuilder.create());
@@ -97,71 +59,6 @@ public class GerritHttpClient implements GerritHttpConnection {
         }
     }
 
-
-    protected static HttpClientBuilder addAuth(final HttpClientBuilder builder, final URI uri, final String username, String password) {
-        if (StringUtils.isNotBlank(username)) {
-            CredentialsProvider provider = new BasicCredentialsProvider();
-            AuthScope scope = new AuthScope(uri.getHost(), uri.getPort(), "realm");
-            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-            provider.setCredentials(scope, credentials);
-            builder.setDefaultCredentialsProvider(provider);
-
-            builder.addInterceptorFirst(new PreemptiveAuth());
-        }
-        return builder;
-    }
-
-    public static class PreemptiveAuth implements HttpRequestInterceptor {
-
-        @Override
-        public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
-            AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
-
-            if (authState.getAuthScheme() == null) {
-                AuthScheme authScheme = (AuthScheme) context.getAttribute("preemptive-auth");
-                CredentialsProvider credsProvider = (CredentialsProvider) context
-                        .getAttribute(ClientContext.CREDS_PROVIDER);
-                HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
-                if (authScheme != null) {
-                    Credentials creds = credsProvider
-                            .getCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort()));
-                    if (creds == null) {
-                        throw new HttpException("No credentials for preemptive authentication");
-                    }
-                    authState.update(authScheme, creds);
-                }
-            }
-        }
-    }
-
-    public CloseableHttpClient getClient() {
-        return client;
-    }
-
-    public void setClient(CloseableHttpClient client) {
-        this.client = client;
-    }
-
-    /**
-     * Get the local context.
-     *
-     * @return context
-     */
-    protected HttpContext getLocalContext() {
-        return localContext;
-    }
-
-
-    /**
-     * Set the local context.
-     *
-     * @param localContext the context
-     */
-    protected void setLocalContext(final HttpContext localContext) {
-        this.localContext = localContext;
-    }
-
-
     /**
      * 获取一个list
      *
@@ -176,17 +73,11 @@ public class GerritHttpClient implements GerritHttpConnection {
     @Override
     public <E extends BaseModel, C extends List>
     List<E> get(String path, Class<E> elementCls, Class<C> listCls) throws IOException {
-        HttpGet getMethod = new HttpGet(UrlUtils.toJsonApiUri(uri, context, path));
-        HttpResponse response = client.execute(getMethod, localContext);
-        try {
-            checkResponse(response);
-            byte[] bytes = getResponseBytes(response);
-            JavaType javaType = mapper.getTypeFactory().constructParametricType(listCls, elementCls);
-            return mapper.readValue(bytes, javaType);
-        } finally {
-            EntityUtils.consume(response.getEntity());
-            releaseConnection(getMethod);
-        }
+        HttpResponse response = request(path, HttpGet.METHOD_NAME, null);
+        byte[] bytes = getResponseBytes(response);
+        JavaType javaType = mapper.getTypeFactory().constructParametricType(listCls, elementCls);
+        return mapper.readValue(bytes, javaType);
+
     }
 
     /**
@@ -205,17 +96,11 @@ public class GerritHttpClient implements GerritHttpConnection {
     @Override
     public <E extends BaseModel, K, M extends Map<K, E>>
     Map<K, E> get(String path, Class<E> elementCls, Class<M> mapCls, Class<K> keyCls) throws IOException {
-        HttpGet getMethod = new HttpGet(UrlUtils.toJsonApiUri(uri, context, path));
-        HttpResponse response = client.execute(getMethod, localContext);
-        try {
-            checkResponse(response);
-            byte[] bytes = getResponseBytes(response);
-            JavaType javaType = mapper.getTypeFactory().constructParametricType(mapCls, keyCls, elementCls);
-            return mapper.readValue(bytes, javaType);
-        } finally {
-            EntityUtils.consume(response.getEntity());
-            releaseConnection(getMethod);
-        }
+        HttpResponse response = request(path, "GET", null);
+        checkResponse(response);
+        byte[] bytes = getResponseBytes(response);
+        JavaType javaType = mapper.getTypeFactory().constructParametricType(mapCls, keyCls, elementCls);
+        return mapper.readValue(bytes, javaType);
     }
 
 
@@ -231,89 +116,31 @@ public class GerritHttpClient implements GerritHttpConnection {
     @Override
     public <E extends BaseModel>
     E get(String path, Class<E> cls) throws IOException {
-        HttpGet getMethod = new HttpGet(UrlUtils.toJsonApiUri(uri, context, path));
-        HttpResponse response = client.execute(getMethod, localContext);
-        try {
-            checkResponse(response);
-            byte[] bytes = getResponseBytes(response);
-            return mapper.readValue(bytes, cls);
-        } finally {
-            EntityUtils.consume(response.getEntity());
-            releaseConnection(getMethod);
-        }
+        HttpResponse response = request(path, "GET", null);
+        checkResponse(response);
+        byte[] bytes = getResponseBytes(response);
+        return mapper.readValue(bytes, cls);
     }
 
     @Override
     public String get(String path) throws IOException {
-        HttpGet getMethod = new HttpGet(UrlUtils.toJsonApiUri(uri, context, path));
-        HttpResponse response = client.execute(getMethod, localContext);
-        try {
-            checkResponse(response);
-            byte[] bytes = getResponseBytes(response);
-            return mapper.readValue(bytes, String.class);
-        } finally {
-            EntityUtils.consume(response.getEntity());
-            releaseConnection(getMethod);
-        }
+        HttpResponse response = request(path, "GET", null);
+        checkResponse(response);
+        byte[] bytes = getResponseBytes(response);
+        return mapper.readValue(bytes, String.class);
     }
 
     @Override
     public String get(String path, boolean raw) throws IOException {
-        HttpGet getMethod = new HttpGet(UrlUtils.toJsonApiUri(uri, context, path));
-        HttpResponse response = client.execute(getMethod, localContext);
-        try {
-            checkResponse(response);
-            byte[] bytes = getResponseBytes(response);
-            if (raw) {
-                return new String(bytes);
-            } else {
-                return mapper.readValue(bytes, String.class);//解析响应体内容,这个是获取字符串内容的。一般json体是一个单个值
-            }
-        } finally {
-            EntityUtils.consume(response.getEntity());
-            releaseConnection(getMethod);
+        HttpResponse response = request(path, "GET", null);
+        checkResponse(response);
+        byte[] bytes = getResponseBytes(response);
+        if (raw) {
+            return new String(bytes);
+        } else {
+            return mapper.readValue(bytes, String.class);//解析响应体内容,这个是获取字符串内容的。一般json体是一个单个值
         }
     }
 
 
-    //从response 中获取 一个字节数组，去除掉json开头的魔术字符串
-    private byte[] getResponseBytes(HttpResponse response) throws IOException {
-        InputStream content = response.getEntity().getContent();
-        byte[] bytes = IOUtils.toByteArray(content);
-        bytes = Arrays.copyOfRange(bytes, MAGIC_PREFIX.length(), bytes.length);
-        return bytes;
-    }
-
-    private void checkResponse(HttpResponse response) throws HttpResponseException {
-        int status = response.getStatusLine().getStatusCode();
-        if (status < 200 || status >= 400) {
-            throw new HttpResponseException(status, response.getStatusLine().getReasonPhrase());
-        }
-        HttpEntity entity = response.getEntity();
-
-        Header contentType = entity.getContentType();
-        if (contentType != null && !contentType.getValue().contains(JSON_MIME_TYPE)) {
-            throw new HttpResponseException(status, String.format("Expected JSON but got '%s'.", contentType.getValue()));
-        }
-    }
-
-
-    private ObjectMapper getDefaultMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        return mapper;
-    }
-
-    private void releaseConnection(HttpRequestBase httpRequestBase) {
-        httpRequestBase.releaseConnection();
-    }
-
-    @Override
-    public void close() throws IOException {
-        try {
-            client.close();
-        } catch (final IOException ex) {
-            LOGGER.debug("I/O exception closing client", ex);
-        }
-    }
 }
